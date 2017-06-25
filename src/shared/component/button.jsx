28a77613766e565d51b3f7a1fe62baec2fe4ecb1
Dilip Kunderu <dilipkunderu@gmail.com>
2017-06-25 13:10:31 -0400

    Bootstrap

--- src/shared/component/button.jsx
+++ src/shared/component/button.jsx
@@ -8,7 +8,13 @@ type Props = {
 }
 
 const button = ({ label, handleClick }: Props) => (
-  <button onClick={handleClick}>{label}</button>
+  <button
+    onClick={handleClick}
+    className="btn btn-primary"
+    type="button"
+  >
+    {label}
+  </button>
 )
 
 export default button
